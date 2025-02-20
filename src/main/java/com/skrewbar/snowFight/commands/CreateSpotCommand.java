0b66b7ce70396594283895b5adeb70dfe154d04f
skuru <skurua1244@gmail.com>
2025-02-20 20:53:34 +0900

    커맨드 프로토타입 생성

---
+++ src/main/java/com/skrewbar/snowFight/commands/CreateSpotCommand.java
@@ -0,0 +1,20 @@
+package com.skrewbar.snowFight.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabCompleter;
+
+import java.util.List;
+
+public class CreateSpotCommand implements CommandExecutor, TabCompleter {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String label, String[] args) {
+        return List.of();
+    }
+}
