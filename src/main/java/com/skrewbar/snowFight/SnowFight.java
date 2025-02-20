43953691de330e8f04c7ec1ba837581727087f89
skuru <skurua1244@gmail.com>
2025-02-20 18:56:39 +0900

    커맨드 구현

--- src/main/java/com/skrewbar/snowFight/SnowFight.java
+++ src/main/java/com/skrewbar/snowFight/SnowFight.java
@@ -3,15 +3,23 @@ package com.skrewbar.snowFight;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class SnowFight extends JavaPlugin {
+    private static SnowFight plugin;
+    public boolean on = false;
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        plugin = this;
+        getServer().getPluginCommand("눈싸움").setExecutor(new ToggleCommand());
+        getServer().getPluginCommand("눈싸움").setTabCompleter(new ToggleCommand());
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+    }
+
+    public static SnowFight getPlugin() {
+        return plugin;
     }
 }
